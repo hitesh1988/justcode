c8f63f4ecfd9cb67ee8519cace88490aa471e211
jasvant <jasvantpal@gmail.com>
2018-02-27 09:43:25 +0530

    working on justcode

--- wp-content/themes/justcode/tp-my-account.php
+++ wp-content/themes/justcode/tp-my-account.php
@@ -1,18 +1,11 @@
-<?php
+<?php 
+
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package justcode
+ * Template Name: My account 
  */
 
-//get_header(); ?>
+
+get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -34,5 +27,5 @@
 	</div><!-- #primary -->
 
 <?php
-//get_sidebar();
-//get_footer();
+get_sidebar();
+get_footer();
